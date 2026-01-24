410af32770e505cd7f01599f16a39311dafdaa25
Roei Oron <roeioron@gmail.com>
2026-01-24 18:35:48 +0200

    nn

--- List.java
+++ List.java
@@ -42,13 +42,16 @@ public class List {
     /** GIVE Textual representation of this list. */
     public String toString() {
         if (size == 0) return "()";
-        String str = "(";
-        ListIterator it = listIterator(0); 
-        while (it.hasNext()) {
-            CharData current = it.next(); 
-            str = str + current.toString() + " ";
+            String str = "(";
+            Node current = first;
+        while (current != null) {
+            str += current.cp.toString(); 
+            if (current.next != null) {
+                str += " "; 
+            }
+            current = current.next;
         }
-        return str.substring(0, str.length() -1) + ")";
+    return str + ")";
     }
 
     /** Returns the index of the first CharData object in this list
@@ -132,12 +135,10 @@ public class List {
 
     /** Returns an iterator over the elements in this list, starting at the given index. */
     public ListIterator listIterator(int index) {
-	    // If the list is empty, there is nothing to iterate   
-	    if (size == 0) return null;
 	    // Gets the element in position index of this list
 	    Node current = first;
 	    int i = 0;
-        while (i < index) {
+        while (i < index && current != null) {
             current = current.next;
             i++;
         }
